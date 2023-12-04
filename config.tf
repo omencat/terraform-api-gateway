5eca4435a09fdb8eb939a4bcf09bd8265146852e
Joe Taormina <taormina@gmail.com>
2023-12-04 14:42:01 -0800

    build: init

---
+++ config.tf
@@ -0,0 +1,7 @@
+terraform {
+  required_version = "= 1.6.4"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
---
+++ main.tf
@@ -0,0 +1,102 @@
+data "archive_file" "code" {
+  type        = "zip"
+  source_dir  = "${path.module}/code"
+  output_path = "${path.module}/code.zip"
+}
+
+data "archive_file" "layer" {
+  type        = "zip"
+  source_dir  = "${path.module}/layer"
+  output_path = "${path.module}/layer.zip"
+  depends_on  = [null_resource.pip_install]
+}
+
+resource "aws_iam_role" "iam_role" {
+  name = "api-lambda-iam-role"
+
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "lambda.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+
+resource "aws_lambda_layer_version" "layer" {
+  layer_name          = "test-api-layer"
+  filename            = data.archive_file.layer.output_path
+  source_code_hash    = data.archive_file.layer.output_base64sha256
+  compatible_runtimes = ["python3.9"]
+}
+
+resource "aws_lambda_function" "lambda" {
+  function_name    = "test-api-lambda"
+  handler          = "lambda.main"
+  runtime          = "python3.9"
+  filename         = data.archive_file.code.output_path
+  source_code_hash = data.archive_file.code.output_base64sha256
+  role             = aws_iam_role.iam_role.arn
+  layers           = [aws_lambda_layer_version.layer.arn]
+  environment {
+    variables = {
+      "MESSAGE" = "Terraform sends its regards"
+    }
+  }
+}
+
+resource "null_resource" "pip_install" {
+  triggers = {
+    shell_hash = "${sha256(file("${path.module}/requirements.txt"))}"
+  }
+
+  provisioner "local-exec" {
+    command = "python3 -m pip install -r requirements.txt -t ${path.module}/layer"
+  }
+}
+
+resource "aws_apigatewayv2_api" "api" {
+  name          = "test-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_stage" "stage" {
+  name        = "$default"
+  auto_deploy = true
+  api_id      = aws_apigatewayv2_api.api.id
+}
+
+resource "aws_apigatewayv2_integration" "integration" {
+  api_id                 = aws_apigatewayv2_api.api.id
+  integration_uri        = aws_lambda_function.lambda.invoke_arn
+  integration_type       = "AWS_PROXY"
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "route" {
+  api_id    = aws_apigatewayv2_api.api.id
+  route_key = "ANY /${aws_lambda_function.lambda.function_name}"
+  target    = "integrations/${aws_apigatewayv2_integration.integration.id}"
+}
+
+resource "aws_lambda_permission" "api" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.arn
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.api.execution_arn}/*/*/*"
+}
+
+resource "aws_iam_role_policy_attachment" "policy" {
+  role       = aws_iam_role.iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
